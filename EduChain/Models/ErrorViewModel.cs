baa22e51e8b0eaecddbdc4d5db75dd0410cd2857
= <ryanneilparker@gmail.com>
2023-08-15 23:02:23 +0200

    Add project files.

---
+++ EduChain/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EduChain.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ EduChain/Views/Home/Verify.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EduChain.Pages
+{
+    public class VerifyCertificateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
