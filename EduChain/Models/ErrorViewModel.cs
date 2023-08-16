503255b0d07f6fea984f88312c06a57b0343fded
= <ryanneilparker@gmail.com>
2023-08-16 21:13:55 +0200

    Clear repository

--- EduChain/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EduChain.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
