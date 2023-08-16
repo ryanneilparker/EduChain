f7f441a660b6333b2accb33e01706497948cb7f6
= <ryanneilparker@gmail.com>
2023-08-16 22:01:19 +0200

    Serve static content

---
+++ index.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`[⚡️]: Server is running at http://localhost:${port}`);
+});
