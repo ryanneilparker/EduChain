694f7909b3de7c0342a7aeac3e99f603e5adbf79
= <ryanneilparker@gmail.com>
2023-09-07 18:41:01 +0200

    Add authentication middleware

--- api/index.routes.ts
+++ api/index.routes.ts
@@ -1,17 +1,22 @@
 import express, { Request, Response, Router } from 'express';
 import path from 'path';
+import { ensureAuthenticated } from '../middleware/auth.middleware';
 
 const index: Router = express.Router();
 
-index.get('/', (req: Request, res: Response) => {
+index.get('/', ensureAuthenticated, (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
-index.get('/create', (req: Request, res: Response) => {
+index.get('/login', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../public', 'login.html'));
+});
+
+index.get('/create', ensureAuthenticated, (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../public', 'create.html'));
 })
 
-index.get('/verify', (req: Request, res: Response) => {
+index.get('/verify', ensureAuthenticated, (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../public', 'verify.html'));
 })
 
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import index from './api/index.routes';
 import web3 from './api/web3.routes';
+import auth from './api/auth.routes';
 import dotenv from 'dotenv';
 import path from 'path';
 
@@ -13,12 +14,16 @@ const port = process.env.PORT;
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static content
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.sendFile(path.join(__dirname, '../public', 'index.html'));
+// });
 
 // API routes
 app.use(index);
 app.use(web3);
-//app.use(auth);
+app.use(auth);
 
 app.listen(port, () => {
   console.log(`[⚡️]: Server is running at http://localhost:${port}`);
